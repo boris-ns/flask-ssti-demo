d41923acce2159c2b2e877ea8a9fdb427a79471a
Boris Sulicenko <b.sulicenko@gmail.com>
2020-12-10 17:10:07 +0100

    Created homepage template and basic form for submiting posts

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'This is super secret key for JWT'
+app.config['SECRET_KEY'] = 'This is super secret key'
 
 
-@app.route("/")
-def index():
-    search = request.args.get('search') or None
+class PostStatusForm(FlaskForm):
+    status_field = StringField('Status', validators=[DataRequired()])
+    submit = SubmitField('Post')
 
-    template = '''
-        <p>Hello world</p>
-        {}
-    '''.format(search)
 
-    return render_template_string(template)
+@app.route('/', methods=['GET', 'POST'])
+def homepage():
+    data = {
+        'name': 'John Smith',
+        'image': ''
+    }
+
+    form = PostStatusForm()
+
+    if form.validate_on_submit():
+        print('You entered {}'.format(form.status_field.data))
+        return redirect('/')
+
+    return render_template('home.html', data=data, form=form)
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
